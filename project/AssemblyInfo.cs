a8b4d662bb80d23cc7a70a545ea8e4e94402f92a
onegrandband <zestyfun@hotmail.com>
2026-09-05 11:46:24 -0500

    Create AssemblyInfo.cs for Mentality AutoClicker
    Added assembly information for Mentality AutoClicker.

---
+++ project/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Mentality AutoClicker")]
+[assembly: AssemblyDescription("Mentality is a configurable auto-click utility designed for automated UI testing and accessibility tasks.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Mentality Solutions")]
+[assembly: AssemblyProduct("Mentality AutoClicker")]
+[assembly: AssemblyCopyright("Copyright ©  2026 Mentality Solutions")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a34e7d86-2ff7-4d3d-909a-967d1c626974")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.2.1.0")]
+[assembly: AssemblyFileVersion("1.2.1.0")]
